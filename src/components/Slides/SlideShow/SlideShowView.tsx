b5c5d6182de26ff466ffc2d0a1a29f07966a2ce5
Adrian Orłów <adrian@orlow.me>
2020-08-15 00:47:27 +0200

    Add SlideShow

---
+++ src/components/Slides/SlideShow/SlideShowView.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import * as R from 'ramda';
+import BannerAnim, { Element } from 'rc-banner-anim';
+import TweenOne from 'rc-tween-one';
+
+import { BasicSlidePartsFragment, ScaledTypes, SlideTypes } from 'types';
+import { Preview } from 'components/Slides';
+import { Scaled } from 'shared';
+import { getBannerTransitionTypeDuration } from './SlideShowUtils';
+
+interface Props {
+  bannerRef: any;
+  size: ScaledTypes.SizeType;
+  slides: BasicSlidePartsFragment[];
+  selectedIndex: number;
+}
+
+class SlideShow extends React.Component<Props> {
+  shouldComponentUpdate(nextProps: Readonly<Props>): boolean {
+    const slidesChanged = R.not(R.equals(nextProps.slides, this.props.slides));
+    const sizeChanged = R.not(R.equals(nextProps.size, this.props.size));
+    const selectedIndexChanged = R.not(R.equals(nextProps.selectedIndex, this.props.selectedIndex));
+    return R.or(
+      R.or(slidesChanged, sizeChanged),
+      selectedIndexChanged,
+    );
+  }
+
+  render() {
+    const { bannerRef, slides, size, selectedIndex } = this.props;
+
+    const selectedSlide = slides[selectedIndex];
+    const transition: SlideTypes.SlideTransition = selectedSlide.transition;
+    const [type, duration] = getBannerTransitionTypeDuration(transition);
+
+    const toElement = (slide: BasicSlidePartsFragment) => (
+      <Element key={slide.id}>
+        <TweenOne>
+          <Scaled key={slide.id} {...size}>
+            <Preview slide={slide}/>
+          </Scaled>
+        </TweenOne>
+      </Element>
+    );
+    const list = R.map(toElement, slides);
+
+    return (
+      <BannerAnim
+        ref={bannerRef}
+        type={type}
+        duration={duration}
+        thumb={false}
+        arrow={false}
+        dragPlay={false}
+        sync
+      >
+        {list}
+      </BannerAnim>
+    );
+  }
+}
+
+export default SlideShow;
