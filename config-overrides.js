9d1080af8f645fe17b68dfac359384da4eb857f0
Adrian Orłów <adrian@orlow.me>
2020-08-18 00:03:00 +0200

    Remove bundle analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,9 @@
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = function override(config, env) {
   config.plugins = [
     ...config.plugins,
     new AntdDayjsWebpackPlugin(),
-    new BundleAnalyzerPlugin(),
   ];
 
   return config;
